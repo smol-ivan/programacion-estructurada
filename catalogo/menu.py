fcb202d8c58d4e7a9027d43ac3433e98e2e17ef0
Ivan Gordillo <gordillosolis@icloud.com>
2023-06-04 15:57:27 -0600

    menu

--- catalogo/menu.py
+++ catalogo/menu.py
@@ -1,40 +1,40 @@
-def menu():
+def options_selection(lista_opciones):
+  option = False
+  while not option:
+      selection = input('Seleccione una opcion(num): ')
+      if selection.isdigit() and int(selection) > 0 and int(selection) <= len(lista_opciones):
+        option = True
+      else: 
+        print('Opcion invalida, intente de nuevo: ')
+  return int(selection)
+
+def options():
+    lista = [{'menu principal': 
+              {1: '~ Agregar un producto.', 
+               2: '~ Buscar un producto.', 
+               3: '~ Eliminar un producto.', 
+               4: '~ Mostrar catálogo.', 
+               5: '~ Cargar catálogo.', 
+               6: '~ Guardar catálogo.', 
+               7: '~ Salir.'}}, 
+             {'menu agregar producto':
+              {1: '~ Película', 
+               2: '~ Serie', 
+               3: '~ Documental', 
+               4: '~ Evento deportivo en vivo',
+               5: '~ Regresar'}}]
+    return lista
+
+def seleccionar_e_imprimir(list_num=0):
+    selected_menu = options()[list_num]
+    menu_title = list(selected_menu.keys())[0]
+    menu_options = selected_menu[menu_title]
     
-
-  """
-  def opciones_menu(num = 0):
-      lista = [{'menu principal': 
-                {1 : '~  Agregar un producto.', 
-                2 : '~  Buscar un produto.', 
-                3 : '~  Eliminar un producto.', 
-                4 : '~  Mostrar catalogo.', 
-                5 : '~  Cargar catalogo.', 
-                6 : '~  Guardar catalogo.', 
-                7 : '~  Salir.'}}, 
-              {'menu agregar producto' :
-                {1 : '~  Pelicula', 
-                2 : '~  Serie', 
-                3 : '~  Documental', 
-                4 : '~  Evento deportivo en vivo',
-                5 : '~  Regresar'}}]
-      return lista[num]
-
-  def imprimir_menu(lista):
-      menu_seleccionado = opciones_menu()
-      menu_titulo = list(menu_seleccionado.keys())[0]
-      menu_opciones = menu_seleccionado[menu_titulo]
-
-      print(f'~ {menu_titulo.capitalize()} ~')
-      for key, value in menu_opciones.items():
+    print(f'~ {menu_title.capitalize()} ~')
+    for key, value in menu_options.items():
         print(key, value)
-  """
 
-def seleccion_opciones(opciones):
-    option = False
-    while not option:
-        selection = input('Seleccione una opcion(num): ')
-        if selection.isdigit() and int(selection) > 0 and int(selection) <= len(opciones):
-            option = True
-        else:
-            print('Opcion invalida, intente de nuevo: ')
-    return int(selection)
+def init():
+   seleccionar_e_imprimir()  # Elige el número correspondiente al menú principal por defecto
+
+init()
